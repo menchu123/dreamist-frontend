cb5cd6612cc9f3864189c128961f64c9f6a925e4
Menchu MV <mcmvillalobos@gmail.com>
2021-11-29 13:27:22 +0100

    Added NavBar HTML

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="nav">
+    <router-link to="/">
+      <font-awesome-icon class="nav__icon nav__icon-book" icon="book"></font-awesome-icon>
+    </router-link>
+    <router-link to="/dream-form">
+      <div class="nav__add-dream-button">+</div>
+    </router-link>
+    <font-awesome-icon class="nav__icon nav__icon-stats" icon="chart-pie"></font-awesome-icon>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavBar",
+});
+</script>
+
+<style lang="scss"></style>
