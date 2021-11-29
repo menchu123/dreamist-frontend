7717c7c33d8e1ee6b0f0209f5f07bb3bbf4e7ac8
Menchu MV <mcmvillalobos@gmail.com>
2021-11-29 12:48:40 +0100

    Styles added to home page

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,48 @@
   <router-view />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "./src/styles/variables";
+#app {
+  font-family: $font-family;
+  letter-spacing: 0.05em;
+  background-color: $darkBackground;
+  box-sizing: border-box;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  color: $lightgrey;
+  min-height: 100vh;
+  width: 100vw;
+  min-height: -webkit-fill-available;
+}
+h1 {
+  color: $lightgrey;
+  font-size: 28px;
+  font-weight: 500;
+  margin: 0 0 10px 0;
+}
+h2 {
+  font-size: 18px;
+  font-weight: normal;
+  line-height: 19px;
+  letter-spacing: 0.05em;
+}
+
+p {
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 120%;
+  letter-spacing: 0.05em;
+  margin: 0 0 7px;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,3 +12,22 @@ export default defineComponent({
   name: "Home",
 });
 </script>
+
+<style lang="scss">
+@import "./src/styles/variables";
+@import "./src/styles/mixins";
+.home {
+  @include backgroundDark;
+  padding: 132px 20px 0;
+  .dream-list {
+    height: calc(100vh - 110px - 150px);
+    overflow-y: scroll;
+    padding-bottom: 30px;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+}
+</style>
