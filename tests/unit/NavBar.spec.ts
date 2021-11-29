b99208f93b223e3d444aca9004b4a06fb49e5d97
Menchu MV <mcmvillalobos@gmail.com>
2021-11-29 13:59:27 +0100

    Tested that navbar renders

---
+++ tests/unit/NavBar.spec.ts
@@ -0,0 +1,18 @@
+import { mount } from "@vue/test-utils";
+import router from "../../src/router";
+import NavBar from "../../src/components/NavBar.vue";
+
+describe("Given a Header component", () => {
+  describe("When is rendered", () => {
+    test("Should render the header html tag", () => {
+      const wrapper = mount(NavBar, {
+        global: {
+          plugins: [router],
+        },
+        stubs: ["router-link", "router-view"],
+      });
+
+      expect(wrapper.html()).toContain('<section class="nav">');
+    });
+  });
+});
