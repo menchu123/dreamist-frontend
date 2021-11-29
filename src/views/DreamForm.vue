2954f6301ea4dff059d6e3b8af327286a7f30f57
Menchu MV <mcmvillalobos@gmail.com>
2021-11-29 17:29:52 +0100

    Added form styles up to categories

--- src/views/DreamForm.vue
+++ src/views/DreamForm.vue
@@ -81,4 +81,142 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "./src/styles/variables";
+@import "./src/styles/mixins";
+
+.form {
+  min-height: 100vh;
+  padding: 0 20px;
+  max-width: $content-width;
+  min-width: 280px;
+  margin: 0 auto;
+  &__top-nav {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    height: 100px;
+    padding-bottom: 15px;
+  }
+  &__label {
+    display: block;
+    font-size: 14px;
+    font-weight: 500;
+    color: #9aa0af;
+    margin-top: 30px;
+    margin-bottom: 10px;
+  }
+  &__input {
+    background-color: transparent;
+    border: none;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 135%;
+    width: 100%;
+    color: $lightgrey;
+    letter-spacing: 0.05em;
+  }
+  &__input::placeholder {
+    color: $blue1;
+  }
+  &__input--description {
+    font-size: 16px;
+    font-weight: 400;
+  }
+  &__input:focus-visible {
+    outline: none;
+  }
+  &__category-buttons {
+    input[type="radio"] {
+      display: none;
+    }
+    input[type="radio"]:checked + .form__category-button-label--normal {
+      background-color: $normal;
+    }
+    input[type="radio"]:checked + .form__category-button-label--lucid {
+      background-color: $lucid;
+    }
+    input[type="radio"]:checked + .form__category-button-label--nightmare {
+      background-color: $nightmare;
+    }
+    input[type="radio"]:checked + .form__category-button-label--daydream {
+      background-color: $daydream;
+    }
+    display: flex;
+    align-items: center;
+    height: 45px;
+    background-color: $blue2;
+    border-radius: 15px;
+    padding: 0 15px;
+    overflow-y: scroll;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  &__category-button-label {
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 15px;
+    text-align: center;
+    border: 1px solid $lightgrey;
+    border-radius: 6px;
+    padding: 2px 8px;
+    margin-right: 10px;
+  }
+  &__back {
+    font-size: 25px;
+  }
+  &__save {
+    @include button;
+  }
+  &__moon {
+    margin-right: 10px;
+  }
+  &__time {
+    width: 100%;
+    background-color: $blue2;
+    border-radius: 15px;
+    font-size: 16px;
+    font-weight: 500;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 10px;
+    .datepicker-toggle {
+      display: inline-block;
+      position: relative;
+      width: 100%;
+      height: 19px;
+      .datepicker-toggle-button {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+      }
+      .datepicker-input {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        cursor: pointer;
+        box-sizing: border-box;
+      }
+      .datepicker-input::-webkit-calendar-picker-indicator {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
