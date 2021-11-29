fee9b7ded54df2e39f936f65d7960f0d534aa150
Menchu MV <mcmvillalobos@gmail.com>
2021-11-29 13:07:25 +0100

    Installed fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 import store from "./store";
+import router from "./router";
+import App from "./App.vue";
 
-createApp(App).use(store).use(router).mount("#app");
+library.add(fas);
+library.add(fab);
+library.add(far);
+
+dom.watch();
+
+createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
