40ef6c87208f2d012e4ec16deae1b31268b65e2e
Menchu MV <mcmvillalobos@gmail.com>
2021-12-01 10:48:50 +0100

    Added loginUser action

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
 import { ActionContext } from "vuex";
-import { State } from "@/types/interfaces";
+import { State, User } from "@/types/interfaces";
 
 const actions = {
   async getDreamsFromApi({ commit }: ActionContext<State, State>): Promise<void> {
@@ -10,6 +11,13 @@ const actions = {
       },
     });
     commit("loadDreams", data);
+  },
+  async loginUser({ commit }: ActionContext<State, State>, user: User): Promise<void> {
+    const { data } = await axios.post(`${process.env.VUE_APP_API_URL}/users/login`, user);
+    const { token } = data;
+    const userInfo = jwtDecode(token);
+    localStorage.setItem("token", JSON.stringify({ token }));
+    commit("loadUser", userInfo);
   },
 };
 
