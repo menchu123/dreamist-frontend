b90853ca95b4fccb0d41acf190aed34ec27cff8b
Menchu MV <mcmvillalobos@gmail.com>
2021-12-01 10:54:39 +0100

    loadUser mutation added

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,8 +1,12 @@
-import { State, Dream } from "@/types/interfaces";
+import { State, Dream, User } from "@/types/interfaces";
 
 const mutations = {
   loadDreams(state: State, payload: Array<Dream>): void {
     state.dreams = payload;
+  },
+  loadUser(state: State, payload: User): void {
+    state.user.user = payload;
+    state.user.isAuthenticated = true;
   },
 };
 
