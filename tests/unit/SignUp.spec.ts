baa0f2df0b43092ecfe5575d20b75938ba2660ba
Menchu MV <mcmvillalobos@gmail.com>
2021-12-01 18:33:36 +0100

    Fixed SignUp test by mocking store

--- tests/unit/SignUp.spec.ts
+++ tests/unit/SignUp.spec.ts
@@ -1,13 +1,21 @@
 import { mount } from "@vue/test-utils";
+import { createStore } from "vuex";
 import router from "../../src/router";
 import SignUp from "../../src/views/SignUp.vue";
+import state from "../mockState";
 
 describe("Given a SignUp component", () => {
   describe("When is rendered", () => {
     test("Should render the section html tag", () => {
+      const store = createStore({
+        state() {
+          return state;
+        },
+      });
+
       const wrapper = mount(SignUp, {
         global: {
-          plugins: [router],
+          plugins: [router, store],
         },
         stubs: ["router-link", "router-view", "FontAwesomeIcon"],
       });
