ef42fa74524ddcdad0dbaad6c5ae50bb6045c832
Menchu MV <mcmvillalobos@gmail.com>
2021-12-03 15:14:26 +0100

    Created Moon component

---
+++ src/components/Moon.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="moon">
+    <transition name="moonrise" mode="out-in">
+      <img v-if="enter" src="@/assets/luna.png" alt="moon" class="moon-image" />
+    </transition>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Moon",
+
+  data() {
+    return {
+      enter: false,
+    };
+  },
+  mounted() {
+    this.enter = true;
+  },
+});
+</script>
+
+<style lang="scss">
+@import "./src/styles/mixins";
+.moon {
+  @include moon-transition;
+}
+</style>
