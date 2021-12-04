e175d02a8c0484feb9229d6f15032105ded94dbf
Menchu MV <mcmvillalobos@gmail.com>
2021-12-05 00:16:06 +0100

    Fixed NavBar test

--- tests/unit/NavBar.spec.ts
+++ tests/unit/NavBar.spec.ts
@@ -1,18 +1,26 @@
 import { mount } from "@vue/test-utils";
+import { createStore } from "vuex";
 import router from "../../src/router";
 import NavBar from "../../src/components/NavBar.vue";
+import state from "../mockState";
 
 describe("Given a NavBar component", () => {
   describe("When is rendered", () => {
     test("Should render the section html tag", () => {
+      const store = createStore({
+        state() {
+          return state;
+        },
+      });
+
       const wrapper = mount(NavBar, {
         global: {
-          plugins: [router],
+          plugins: [router, store],
         },
         stubs: ["router-link", "router-view", "FontAwesomeIcon"],
       });
 
-      expect(wrapper.html()).toContain('<section class="nav">');
+      expect(wrapper.html()).toContain('<nav class="nav">');
     });
   });
 });
