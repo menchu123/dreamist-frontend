df3a3406e22ab926106231ace64ed7792c99bb79
Menchu MV <mcmvillalobos@gmail.com>
2021-12-05 11:37:42 +0100

    Fixed warnings in SignUp test

--- tests/unit/SignUp.spec.ts
+++ tests/unit/SignUp.spec.ts
@@ -1,16 +1,54 @@
 import { mount } from "@vue/test-utils";
 import { createStore } from "vuex";
-import router from "../../src/router";
+import { createRouter, createWebHistory } from "vue-router";
 import SignUp from "../../src/views/SignUp.vue";
 import state from "../mockState";
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: {
+        template: "Welcome to the blogging app",
+      },
+    },
+    {
+      path: "/login",
+      component: {
+        template: "Welcome to the blogging app",
+      },
+    },
+  ],
+});
+
 describe("Given a SignUp component", () => {
   describe("When is rendered", () => {
-    test("Should render the section html tag", () => {
+    test("Should render the section html tag", async () => {
       const store = createStore({
         state() {
           return state;
         },
+        actions: { checkToken: jest.fn() },
+      });
+
+      const wrapper = mount(SignUp, {
+        global: {
+          plugins: [router, store],
+        },
+        stubs: ["router-link", "router-view"],
+      });
+      await router.isReady();
+      expect(wrapper.html()).toContain('<section class="signup">');
+    });
+  });
+  describe("When the form is submitted", () => {
+    test("Then it should invoke onSubmit", () => {
+      const store = createStore({
+        state() {
+          return state;
+        },
+        actions: { loginUser: jest.fn(), checkToken: jest.fn() },
       });
 
       const wrapper = mount(SignUp, {
@@ -20,7 +58,11 @@ describe("Given a SignUp component", () => {
         stubs: ["router-link", "router-view"],
       });
 
-      expect(wrapper.html()).toContain('<section class="signup">');
+      const onSubmit = jest.fn();
+      onSubmit();
+      const form = wrapper.get("form");
+      form.trigger("submit");
+      expect(onSubmit).toHaveBeenCalled();
     });
   });
 });
