2f00523cc7e2208eb2e8f741d8b3dc75fa535bb9
Menchu MV <mcmvillalobos@gmail.com>
2021-12-07 22:11:06 +0100

    Light styles applied to Moon component

--- src/components/Moon.vue
+++ src/components/Moon.vue
@@ -1,13 +1,22 @@
 <template>
   <section class="moon">
     <transition name="moonrise" mode="out-in">
-      <img height="80" v-if="enter" src="@/assets/luna.png" alt="moon" class="moon-image" />
+      <img
+        @click="toggleLight"
+        height="80"
+        v-if="enter"
+        :src="require(`@/assets/${myImageSource}.png`)"
+        alt="moon"
+        key="moon-sun"
+        class="moon-image"
+      />
     </transition>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { mapActions, mapState } from "vuex";
 
 export default defineComponent({
   name: "Moon",
@@ -15,7 +24,19 @@ export default defineComponent({
   data() {
     return {
       enter: false,
+      imgSourceDark: "luna",
+      imgSourceLight: "sol",
     };
+  },
+  computed: {
+    ...mapState(["user", "isLight"]),
+
+    myImageSource() {
+      return this.isLight ? this.imgSourceLight : this.imgSourceDark;
+    },
+  },
+  methods: {
+    ...mapActions(["toggleLight"]),
   },
   mounted() {
     this.enter = true;
